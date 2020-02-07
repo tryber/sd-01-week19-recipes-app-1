db497c4b7a03ed82d7fde8e90c9c356e686d0092
vrramosm <vf.ramos7@gmail.com>
2020-02-07 19:09:25 -0300

    Fixing CC

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
       <Header />
       <h1>Home</h1>
     </div>
-  )
+  );
 }
 
 export default Home;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function Profile() {
   return (
     <div>
-    <h1>Profile</h1>
+      <h1>Profile</h1>
     </div>
-  )
+  );
 }
 
 export default Profile;
