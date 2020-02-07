7a921ed4aba07150b1b3947bbfaa75b0beba091d
vrramosm <vf.ramos7@gmail.com>
2020-02-07 19:06:56 -0300

    Finalizando o componente header

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import IconProfile from '../img/iconProfile.png';
+import IconSearch from '../img/iconSearch.png';
+
+import '../styles/Header.css';
+
+function Header() {
+  return (
+    <div className="separating-the-icons">
+      <Link to="/profile"><img src={IconProfile} alt="Icon Profiler" /></Link>
+      <h1>Header</h1>
+      <img src={IconSearch} alt="Icon to Search" />
+    </div>
+  );
+}
+
+export default Header;
