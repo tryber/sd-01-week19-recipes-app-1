c66e31bb5ffa32558826903124cdc9957683a09a
richardwellerson <richardwellerson@gmail.com>
2020-02-08 12:39:05 -0300

    Modificando nome e funçoes da API para first lower case

--- src/services/drinkAPI.js
+++ src/services/drinkAPI.js
@@ -1,8 +1,8 @@
 const linkCocktailAPI = 'https://thecocktaildb.com/api/json/v1/1/';
-const CocktailAPI = (value) => (
+const cocktailAPI = (value) => (
   fetch(`${linkCocktailAPI}${value}`)
     .then((response) => response.json()
       .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))))
 );
 
-export default CocktailAPI;
+export default cocktailAPI;
--- src/services/foodAPI.js
+++ src/services/foodAPI.js
@@ -1,8 +1,8 @@
 const linkMeatAPI = 'https://www.themealdb.com/api/json/v1/1/';
-const MeatAPI = (value) => (
+const meatAPI = (value) => (
   fetch(`${linkMeatAPI}${value}`)
     .then((response) => response.json()
       .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))))
 );
 
-export default MeatAPI;
+export default meatAPI;
