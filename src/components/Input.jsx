4dc8a7c6efab402e9d23b1ca4f84436a12d9cbb3
richardwellerson <richardwellerson@gmail.com>
2020-02-10 16:37:30 -0300

    adicionando debounce

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import meatAPI from '../services/foodAPI';
 import cocktailAPI from '../services/drinkAPI';
 
-const isURL = (inputValue) => {
+const validateURL = window.location.href;
+
+const isURL = debounce((inputValue) => {
   if (/meals/.test(window.location.href)) {
     return meatAPI(inputValue);
   } else if (/cocktails/.test(window.location.href)) {
     return cocktailAPI(inputValue);
   }
   return '';
-};
+}, 600);
 
 const inputHeader = () => (
   <div>
