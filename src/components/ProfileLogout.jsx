504e00f3979da2be067a29fc022a050cd2b45881
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-02-11 17:32:52 -0300

    finalizando tela de perfil

---
+++ src/components/ProfileLogout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ProfileLogout = () => (
+  <Link to="/">
+    <button
+      className="button" 
+      data-testid="profile-logout-btn" 
+      onClick={() => localStorage.removeItem('user')}
+    >
+      Sair
+    </button>
+  </Link>
+);
+
+export default ProfileLogout;
---
+++ src/components/ProfilePage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ProfileLogout from './ProfileLogout';
+
+import '../styles/ProfilePage.css'
+
+
+const ProfilePage = () => {
+  const userEmail = JSON.parse(localStorage.getItem('user'));
+  return (
+    <div className="every-elements">
+      <p data-testid="profile-email">{userEmail.email}</p>
+      <Link to="/receitas-feitas">
+        <button className="button" data-testid="profile-done-btn">Receitas Feitas</button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button className="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
+      </Link>
+      <ProfileLogout />
+    </div>
+  );
+};
+
+export default ProfilePage;
