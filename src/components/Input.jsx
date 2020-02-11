5183aa2597b65345ec607dd41704ed3c7d9c1947
richardwellerson <richardwellerson@gmail.com>
2020-02-11 16:56:27 -0300

    Adicionando debounce input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
+import { DebounceInput } from 'react-debounce-input';
 import meatAPI from '../services/foodAPI';
 import cocktailAPI from '../services/drinkAPI';
 
 const validateURL = window.location.href;
 
-const isURL = debounce((inputValue) => {
-  if (/meals/.test(window.location.href)) {
-    return meatAPI(inputValue);
-  } else if (/cocktails/.test(window.location.href)) {
-    return cocktailAPI(inputValue);
-  }
-  return '';
-}, 600);
+const []
+
+const isURL = (inputValue) => {
+
+};
 
 const inputHeader = () => (
   <div>
-    <input
+    <DebounceInput
+
       placeholder="Buscar receita"
       onChange={(e) => isURL(e.target.value)}
     />
