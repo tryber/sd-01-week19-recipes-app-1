60db842d9f79d6747bf1b6b0aa39318ff111a520
richardwellerson <richardwellerson@gmail.com>
2020-02-12 16:34:35 -0300

    retirando return

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -15,35 +15,33 @@ const InputSearch = () => {
   );
 };
 
-const InputHeader = () => {
-  return (
-    <div className="Header_input">
-      <InputSearch />
-      <div>
-        <label htmlFor="ingredients_radio-header">Ingrediente</label>
-        <input
-          data-testid="ingredient-search-radio"
-          type="radio"
-          name="Radio_input-search"
-          id="ingredients_radio-header"
-        />
-        <label htmlFor="name_radio-header">Nome</label>
-        <input
-          data-testid="name-search-radio"
-          type="radio"
-          name="Radio_input-search"
-          id="name_radio-header"
-        />
-        <label htmlFor="first-letter_radio-header">Primeira Letra</label>
-        <input
-          data-testid="first-letter-search-radio"
-          type="radio"
-          name="Radio_input-search"
-          id="first-letter_radio-header"
-        />
-      </div>
+const InputHeader = () => (
+  <div className="Header_input">
+    <InputSearch />
+    <div>
+      <label htmlFor="ingredients_radio-header">Ingrediente</label>
+      <input
+        data-testid="ingredient-search-radio"
+        type="radio"
+        name="Radio_input-search"
+        id="ingredients_radio-header"
+      />
+      <label htmlFor="name_radio-header">Nome</label>
+      <input
+        data-testid="name-search-radio"
+        type="radio"
+        name="Radio_input-search"
+        id="name_radio-header"
+      />
+      <label htmlFor="first-letter_radio-header">Primeira Letra</label>
+      <input
+        data-testid="first-letter-search-radio"
+        type="radio"
+        name="Radio_input-search"
+        id="first-letter_radio-header"
+      />
     </div>
-  );
-};
+  </div>
+);
 
 export default InputHeader;
