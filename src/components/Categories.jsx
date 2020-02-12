9a8b2f4edf73620fae003ba39835c0d77239939d
antoniopaolillo <antonio_francisco_p@live.com>
2020-02-12 13:58:33 -0300

    finalizando botões de categorias

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,18 +1,37 @@
 import React, { useContext } from 'react';
 import context from '../context/context';
 import { mealAPI } from '../services/foodAPI';
+import generateRandomLetter from '../services/randomLetter';
+import './Categories.css';
 
 function updateAPI(category, setResult) {
   mealAPI(`filter.php?c=${category}`, setResult);
 }
 
+function buttonAll(setResult) {
+  return (
+    <button
+    className="category-button"
+      data-testid="all-category-filter"
+      type="button"
+      onClick={() =>
+        mealAPI(`search.php?s=${generateRandomLetter()}`, setResult)
+      }
+    >
+      All
+    </button>
+  );
+}
+
 function generateCategories(categories, setResult) {
   return (
-    <div>
+    <div className="category-container">
+      {buttonAll(setResult)}
       {categories.map((category) => {
         const { strCategory } = category;
         return (
           <button
+            className="category-button"
             key={strCategory}
             data-testid={`${strCategory}-category-filter`}
             type="button"
@@ -25,9 +44,12 @@ function generateCategories(categories, setResult) {
     </div>
   );
 }
+
 function Categories() {
   const { mealsCategory, setResult } = useContext(context);
-  return <div>{generateCategories(mealsCategory.meals.slice(0, 5), setResult)}</div>;
+  return (
+    <div>{generateCategories(mealsCategory.meals.slice(0, 5), setResult)}</div>
+  );
 }
 
 export default Categories;
