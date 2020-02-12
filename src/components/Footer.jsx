965b138b50e075ab15615b3f6b848d2480d37cae
richardwellerson <richardwellerson@gmail.com>
2020-02-12 16:58:38 -0300

    modificando caminho css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,29 +3,31 @@ import { Link } from 'react-router-dom';
 import drinkIcon from '../img/drinkIcon.svg';
 import explorerIcon from '../img/explorerIcon.svg';
 import foodIcon from '../img/foodIcon.svg';
-import './Footer.css';
-
+import '../styles/Footer.css';
 
 const Footer = () => (
   <div className="Footer_father">
-    <Link to="/cocktails">
+    <Link to="/bebidas">
       <img
         className={'Footer_icon'}
-        alt={'footer icon'}
+        data-testid="drinks-bottom-btn"
+        alt={'drinks icon'}
         src={drinkIcon}
       />
     </Link>
-    <Link to="/explore">
+    <Link to="/explorar">
       <img
         className={'Footer_icon'}
-        alt={'footer icon'}
+        data-testid="explore-bottom-btn"
+        alt={'explore icon'}
         src={explorerIcon}
       />
     </Link>
-    <Link to="/meals">
+    <Link to="/comidas">
       <img
         className={'Footer_icon'}
-        alt={'footer icon'}
+        data-testid="food-bottom-btn"
+        alt={'meals icon'}
         src={foodIcon}
       />
     </Link>
