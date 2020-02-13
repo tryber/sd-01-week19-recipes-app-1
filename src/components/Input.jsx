18a745cb9ca3032956bda6318d10651dfc86a07b
antoniopaolillo <antonio_francisco_p@live.com>
2020-02-13 17:56:59 -0300

    adicionando estilização pro input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -7,6 +7,7 @@ const InputSearch = () => {
   const { setSearchInput } = useContext(context);
   return (
     <DebounceInput
+    className="text-input"
       data-testid="search-input"
       debounceTimeout={600}
       placeholder="Buscar receita"
@@ -16,28 +17,28 @@ const InputSearch = () => {
 };
 
 const RadioToSearch = () => (
-  <div>
-    <label htmlFor="ingredients_radio-header">Ingrediente</label>
+  <div className="radios-input">
     <input
       data-testid="ingredient-search-radio"
       type="radio"
       name="Radio_input-search"
       id="ingredients_radio-header"
     />
-    <label htmlFor="name_radio-header">Nome</label>
+    <label htmlFor="ingredients_radio-header">Ingrediente</label>
     <input
       data-testid="name-search-radio"
       type="radio"
       name="Radio_input-search"
       id="name_radio-header"
     />
-    <label htmlFor="first-letter_radio-header">Primeira Letra</label>
+    <label htmlFor="name_radio-header">Nome</label>
     <input
       data-testid="first-letter-search-radio"
       type="radio"
       name="Radio_input-search"
       id="first-letter_radio-header"
     />
+    <label htmlFor="first-letter_radio-header">Primeira Letra</label>
   </div>
 );
 
