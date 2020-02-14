6abbfbd13efb98ef76022b29c7c7edea323fcd04
vrramosm <vf.ramos7@gmail.com>
2020-02-14 19:02:33 -0300

    Fixing CC

--- src/components/CopyToClipboard.jsx
+++ src/components/CopyToClipboard.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import useClipboard from "react-use-clipboard";
+import useClipboard from 'react-use-clipboard';
 
 import 'remixicon/fonts/remixicon.css';
 
 export default function ShareRecipe() {
   const [setCopied] = useClipboard(window.location.href);
-  
+
   return (
-    <i class="ri-share-line ri-xl" onClick={setCopied}></i>
+    <i className="ri-share-line ri-xl" onClick={setCopied} />
   );
 }
