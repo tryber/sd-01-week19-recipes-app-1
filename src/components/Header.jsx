ff41c70d7f568baf992ce8950c1559ae2fa0fd66
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-02-14 18:15:26 -0300

    Code Climate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import IconProfile from '../img/iconProfile.png';
 import IconSearch from '../img/iconSearch.png';
