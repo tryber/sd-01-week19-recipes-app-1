c4e9a6b328f20ab3ad389c782f121ba9bcbe685c
richardwellerson <richardwellerson@gmail.com>
2020-02-14 16:11:17 -0300

    Estrutura criada. Criar componente

--- src/components/IngredientsExploreScreen.jsx
+++ src/components/IngredientsExploreScreen.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
-const IngredientsExploreScreen = (...drinkOrMeal) => {
-  const divWithIngredient = () => {
-    drinkOrMeal.map(property => (
-      //Fazer um request na API com vários ingredientes
-      // De acordo com o toque do usuário, redirecionar para uma página que contenha receitas
-      // com esse ingrediente escolhido
-    ))
-  };
-};
+const IngredientsExploreScreen = () => (
+  <div>
+    <Header />
+    <IngredientsFromAPI />
+    <Footer />
+  </div>
+);
 
 export default IngredientsExploreScreen;
