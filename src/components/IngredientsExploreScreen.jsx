8f455cffb131bf560f6a0fd1d82e83c12b61bd45
richardwellerson <richardwellerson@gmail.com>
2020-02-14 14:51:16 -0300

    subindo logica comentada para nao esquecer

--- src/components/IngredientsExploreScreen.jsx
+++ src/components/IngredientsExploreScreen.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const IngredientsExploreScreen = () => (
-  <div>Aqui</div>
-);
+const IngredientsExploreScreen = (...drinkOrMeal) => {
+  const divWithIngredient = () => {
+    drinkOrMeal.map(property => (
+      //Fazer um request na API com vários ingredientes
+      // De acordo com o toque do usuário, redirecionar para uma página que contenha receitas
+      // com esse ingrediente escolhido
+    ))
+  };
+};
 
 export default IngredientsExploreScreen;
