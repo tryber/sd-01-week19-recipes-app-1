0665ec06007ac588e42eb89b52e1782b37d8a8b7
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-02-15 19:05:55 -0300

    Finaliando tela de explorar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import ProfilePage from './components/ProfilePage';
 import FoodPage from './components/FoodPage';
+import ExplorePage from './components/ExplorePage';
 
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
         <Route exact path="/bebidas" />
         <Route path="/receitas-feitas" />
         <Route path="/receitas-favoritas" />
-        <Route exact path="/explorar" />
+        <Route exact path="/explorar" component={ExplorePage} />
         <Route exact path="/explorar/comidas" />
         <Route exact path="/explorar/bebidas" />
         <Route path="/explorar/comidas/ingredientes" />
--- src/components/ExplorePage.jsx
+++ src/components/ExplorePage.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 
-const ExplorePage = () => {
+import context from '../context/context';
+import Header from './Header';
+import Footer from './Footer';
+import '../styles/ExplorePage.css';
+
+const ExplorePage = ({ location: { pathname } }) => {
+  const { setCurrentFood } = useContext(context);
+
+  useEffect(() => {
+    if (pathname === '/explorar') {
+      setCurrentFood('Explorar');
+    }
+  }, [pathname]);
+
   return (
     <div>
-      <Link to="/explorar/comidas">
-        <button>Explorar Comidas</button>
-      </Link>
-      <Link to="/explorar/bebidas">
-        <button>Explorar Comidas</button>
-      </Link>
+      <Header />
+      <div className="center-buttons">
+        <Link to="/explorar/comidas">
+          <button data-testid="explore-food" className="button">Explorar Comidas</button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <button data-testid="explore-drinks" className="button">Explorar Comidas</button>
+        </Link>
+      </div>
+      <div className="explore-footer">
+        <Footer />
+      </div>
     </div>
   );
 };
