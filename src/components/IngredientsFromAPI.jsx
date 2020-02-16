e8dcb8bd5950db9a2b814609cb5cd929416e6c79
richardwellerson <richardwellerson@gmail.com>
2020-02-16 13:14:48 -0300

    Criado componente

--- src/components/IngredientsFromAPI.jsx
+++ src/components/IngredientsFromAPI.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import { mealAPI, drinkAPI } from '../services/foodAPI';
 
-const IngredientsFromAPI = ({pathname}) => {
 
+
+const IngredientsFromAPI = ({ ingredients, pathname, index }) => {
+  ingredients.map((ingredient) => {
+    if (pathname.includes('comidas')) {
+      const ingredientName = ingredient.strIngredient;
+      const ingredientImage = `https://www.themealdb.com/images/ingredients/${ingredientName}.png`;
+      return (
+        <div>
+          <div>
+            <img
+              src={ingredientImage}
+              data-testid={`${ingredientName}-card-img`}
+            />
+            <p>Ingredient</p>
+            <p>{ingredientName}</p>
+          </div>
+        </div>
+      );
+    }
+  }
 }
 
 export default IngredientsFromAPI;
