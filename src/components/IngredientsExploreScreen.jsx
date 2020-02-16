8c97181d55ce949f996bc2dadd8b06a782fd44ea
richardwellerson <richardwellerson@gmail.com>
2020-02-16 14:56:37 -0300

    Alterando comportamento e inserindo data-testid nas tags

--- src/components/IngredientsExploreScreen.jsx
+++ src/components/IngredientsExploreScreen.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import IngredientsFromAPI from './IngredientsFromAPI';
+import context from '../context/context';
 import '../styles/ingredientsExploreScreen.css';
 
-const IngredientsExploreScreen = () => (
-  <div className="Explore_Ingredients-father">
-    <Header />
-    <IngredientsFromAPI />
-    <div className="Explore_Ingredients-footer">
-      <Footer />
+const IngredientsExploreScreen = () => {
+  const { ingredient, setIngredient } = useContext(context);
+  return (
+    <div className="Explore_Ingredients-father">
+      <Header />
+      <IngredientsFromAPI
+        ingredient={ingredient}
+        setIngredient={setIngredient}
+      />
+      <div className="Explore_Ingredients-footer">
+        <Footer />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default IngredientsExploreScreen;
--- src/components/IngredientsFromAPI.jsx
+++ src/components/IngredientsFromAPI.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const IngredientsFromAPI = ({ ingredients, pathname }) => {
   ingredients.map((ingredient) => {
     const ingredientName = ingredient.strIngredient1;
-    const ingredientImage = `https://www.thecocktaildb.com/images/ingredients/${ingredientName}.png`;
+    let ingredientImage = `https://www.thecocktaildb.com/images/ingredients/${ingredientName}.png`;
     if (pathname.includes('comidas')) {
       const ingredientName = ingredient.strIngredient;
-      const ingredientImage = `https://www.themealdb.com/images/ingredients/${ingredientName}.png`;
+      ingredientImage = `https://www.themealdb.com/images/ingredients/${ingredientName}.png`;
     }
     return (
       <div>
@@ -15,12 +15,14 @@ const IngredientsFromAPI = ({ ingredients, pathname }) => {
             src={ingredientImage}
             data-testid={`${ingredientName}-card-img`}
           />
-          <div>
-            <p>Ingredient</p>
-            <p>{ingredientName}</p>
-          </div>
         </div>
-      </div>
+        <div>
+          <p>Ingredient</p>
+          <p data-testid={`${ingredientName}-card-name`}>
+            {ingredientName}
+          </p>
+        </div>
+      </div >
     );
   });
 }
