3cb28f79355dc2e62ac1aed67451f05d1339018a
antoniopaolillo <antonio_francisco_p@live.com>
2020-02-27 15:17:25 -0300

    iniciando criação da pagina de progresso

--- src/components/IngredientsFromAPI.jsx
+++ src/components/IngredientsFromAPI.jsx
@@ -10,6 +10,7 @@ const IngredientsFromAPI = ({ ingredient, currentFood }) => {
       ingredientName = unity.strIngredient;
       ingredientImage = `https://www.themealdb.com/images/ingredients/${ingredientName}.png`;
     }
+    //asuhd
     return (
       <Link to="/receitas/comidas/">
         <div className="Ingredients_container">
